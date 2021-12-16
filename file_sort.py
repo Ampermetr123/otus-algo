3d5e3078b9725c7efeee2958292b45acb5b2b65e
Ampermetr123 <sb.simonov@gmail.com>
2021-12-16 17:32:21 +0300

    refactor, fix mergeheap, test & report updated

--- file_sort.py
+++ file_sort.py
@@ -9,7 +9,8 @@ from sort_selection import heap_sort
 
 
 class FileSort:
-    '''Base class with helpler functions'''    
+    '''Base class with helpler functions'''
+
     def __init__(self,  filename, item_size=2):
         self.item_sz = item_size
         if item_size == 2:
@@ -18,45 +19,54 @@ class FileSort:
             self.item_fmt = 'B'
         else:
             raise RuntimeError(f'Item size {item_size} not supported')
-        
-        self.filename = filename
-        self.fd = os.open(filename, flags=os.O_RDWR | os.O_BINARY )
-        self.size = os.fstat(self.fd).st_size // self.item_sz
-        
 
-    def _read_bytes_from(self, i, size=1):
-        os.lseek(self.fd, i*self.item_sz, os.SEEK_SET)
-        bytes = os.read(self.fd, 2 * size)
+        self.filename = filename
+        fd = os.open(filename, flags=os.O_RDWR | os.O_BINARY)
+        self.size = os.fstat(fd).st_size // self.item_sz
+        os.close(fd)
+
+    def _read_bytes_from(self, fd, i, size=1):
+        os.lseek(fd, i*self.item_sz, os.SEEK_SET)
+        bytes = os.read(fd, self.item_sz * size)
         return bytes
 
-    def _write_bytes_to(self, i, bytes):
-        os.lseek(self.fd, i*self.item_sz, os.SEEK_SET)
-        os.write(self.fd, bytes)
+    def _write_bytes_to(self, fd, i, bytes):
+        os.lseek(fd, i*self.item_sz, os.SEEK_SET)
+        os.write(fd, bytes)
 
-    def __del__(self):
-        os.close(self.fd)
+    def sort(self):
+        fd = os.open(self.filename, flags=os.O_RDWR | os.O_BINARY)
+        try:
+            self._sort_impl(fd, 0, self.size-1)
+        finally:
+            os.close(fd)
 
 
 ################################################################################
 #               CLASS FOR SORTING FILE WITH MERGE SORT ALGORITHM               #
 ################################################################################
 
-
 class FileSortMerge(FileSort):
+    ''' File sort (in-place) with merge algo'''
+    RAM_ITEMS_LIMIT = 64*1024
 
     def __init__(self, filename, item_size=2):
         super().__init__(filename, item_size=item_size)
 
-    def _merge_big(self, ls, m, rs):
+    def _merge_big(self, fd, ls, m, rs):
         '''Merge [ls, m) and [m, rs ]  -> [ls, rs]; temp file is used'''
         i, j = ls, m
-        tfd = os.open(f'{self.filename}.merging.tmp', # flags for Windows host
+        try:
+            tfd = os.open(f'{self.filename}.merging-1.tmp',  # flags for Windows host
+                      flags=os.O_RDWR | os.O_BINARY | os.O_TEMPORARY | os.O_CREAT | os.O_SEQUENTIAL)
+        except PermissionError:
+            tfd = os.open(f'{self.filename}.merging-2.tmp',  # flags for Windows host
                       flags=os.O_RDWR | os.O_BINARY | os.O_TEMPORARY | os.O_CREAT | os.O_SEQUENTIAL)
 
         while i < m and j <= rs:
-            di_bytes = self._read_bytes_from(i)
+            di_bytes = self._read_bytes_from(fd, i)
             di = unpack(self.item_fmt, di_bytes)
-            dj_bytes = self._read_bytes_from(j)
+            dj_bytes = self._read_bytes_from(fd, j)
             dj = unpack(self.item_fmt, dj_bytes)
             if di <= dj:
                 os.write(tfd, di_bytes)
@@ -65,26 +75,26 @@ class FileSortMerge(FileSort):
                 os.write(tfd, dj_bytes)
                 j += 1
         if i < m:
-            os.write(tfd, self._read_bytes_from(i, m - i))
+            os.write(tfd, self._read_bytes_from(fd, i, m - i))
         if j <= rs:
-            os.write(tfd, self._read_bytes_from(j, rs - j + 1))
+            os.write(tfd, self._read_bytes_from(fd, j, rs - j + 1))
 
-        os.lseek(tfd, 0, os.SEEK_SET)
-        raw = os.read(tfd, (rs - ls + 1) * self.item_sz) # Note: in Linux would use os.copy_file_range
-        os.lseek(self.fd, ls * self.item_sz, os.SEEK_SET)
-        os.write(self.fd, raw)
+        raw = self._read_bytes_from(tfd, 0, (rs - ls + 1))
+        self._write_bytes_to(fd, ls, raw)  # Note: in Linux better use os.copy_file_range
+
+        os.fsync(tfd)
         os.close(tfd)
 
 
-    def _merge_small(self, ls, m, rs):
+    def _merge_small(self, fd, ls, m, rs):
         '''Merge [ls, m) and [m, rs ]  -> [ls, rs]; in RAM list is used'''
         i, j = ls, m
         s = []
 
         while i < m and j <= rs:
-            di_bytes = self._read_bytes_from(i)
+            di_bytes = self._read_bytes_from(fd, i)
             di = unpack(self.item_fmt, di_bytes)
-            dj_bytes = self._read_bytes_from(j)
+            dj_bytes = self._read_bytes_from(fd, j)
             dj = unpack(self.item_fmt, dj_bytes)
             if di <= dj:
                 s.append(di_bytes)
@@ -93,45 +103,28 @@ class FileSortMerge(FileSort):
                 s.append(dj_bytes)
                 j += 1
         while i < m:
-            di_bytes = self._read_bytes_from(i)
+            di_bytes = self._read_bytes_from(fd, i)
             s.append(di_bytes)
             i += 1
         while j <= rs:
-            dj_bytes = self._read_bytes_from(j)
+            dj_bytes = self._read_bytes_from(fd, j)
             s.append(dj_bytes)
             j += 1
 
-        os.lseek(self.fd, ls * self.item_sz, os.SEEK_SET)
+        os.lseek(fd, ls * self.item_sz, os.SEEK_SET)
         for x in s:
-            os.write(self.fd, x)
+            os.write(fd, x)
 
-
-    def _sort_recursive(self, ls, rs):
+    def _sort_impl(self, fd, ls, rs):
         ''' Recursive sort d[ls,rs]'''
         if (size := rs-ls) >= 1:
             m = (ls + rs) // 2
-            self._sort_recursive(ls, m)
-            self._sort_recursive(m+1, rs)
-            if (size > 524288):
-                self._merge_big(ls, m+1, rs)
+            self._sort_impl(fd, ls, m)
+            self._sort_impl(fd, m+1, rs)
+            if (size > self.RAM_ITEMS_LIMIT):
+                self._merge_big(fd, ls, m+1, rs)
             else:
-                self._merge_small(ls, m+1, rs)
-
-
-    def _sort_impl(self, optMode=0):
-        recursion_limit = int(log2(self.size))+100
-        limit = sys.getrecursionlimit()
-        sys.setrecursionlimit(max(limit, recursion_limit))
-        self._sort_recursive(0, self.size-1)
-        sys.setrecursionlimit(limit)
-
-
-    def sort(self):
-        recursion_limit = int(log2(self.size))+1
-        threading.stack_size(32000 + 768 * recursion_limit)
-        thread = threading.Thread(target=self._sort_impl)
-        thread.start()
-        thread.join()
+                self._merge_small(fd, ls, m+1, rs)
 
 
 ################################################################################
@@ -139,95 +132,101 @@ class FileSortMerge(FileSort):
 ################################################################################
 
 class FileSortMergeHeap(FileSortMerge):
+    ''' File sort (in-place) with merge + heap algo'''
     def __init__(self, filename, item_size=2):
         super().__init__(filename, item_size=item_size)
-    
-    def _sort_recursive(self, ls, rs):
+
+    def _sort_impl(self, fd, ls, rs):
         ''' Recursive sort d[ls,rs]'''
         if (size := rs-ls) >= 1:
             m = (ls + rs) // 2
-            if (size > 524288):
-                self._sort_recursive(ls, m)
-                self._sort_recursive(m+1, rs)
-                self._merge_big(ls, m+1, rs)
+            if (size > self.RAM_ITEMS_LIMIT):
+                self._sort_impl(fd, ls, m)
+                self._sort_impl(fd, m+1, rs)
+                self._merge_big(fd, ls, m+1, rs)
             else:
-                self._heap_sort(ls, rs)
-                self._merge_small(ls, m+1, rs)
-    
-    def _heap_sort(self, ls, rs):
+                self._heap_sort(fd, ls, rs)
+
+    def _heap_sort(self, fd, ls, rs):
         # read [ls, rs] to array
         d = []
-        # Point for optimization - read and write by chunks(blocks)
-        for i in range (rs - ls + 1):
-            buf =self._read_bytes_from(i)
-            d.append( unpack(self.item_fmt, buf)[0] )
-        
+
+        for i in range(ls, rs+1, 1):
+            buf = self._read_bytes_from(fd, i)
+            d.append(unpack(self.item_fmt, buf)[0])
+
         heap_sort(rs-ls+1, d)
 
-        os.lseek(self.fd, ls * self.item_sz, os.SEEK_SET)
+        os.lseek(fd, ls * self.item_sz, os.SEEK_SET)
         for x in d:
-            os.write(self.fd, pack("H", x))
+            raw = pack("H", x)
+            os.write(fd,raw )
+        
+      
 
 
 ################################################################################
-#               CLASS FOR SORTING FILE WITH COUNTING ALGO                      #
+#               CLASSES FOR SORTING FILE WITH COUNTING ALGO                      #
 ################################################################################
 
 class FileSortCounting(FileSortMerge):
+    ''' File sort (in-place) with counting algo'''
     def __init__(self, filename, item_size=2):
         super().__init__(filename, item_size=item_size)
-    
-    def _count(self):
-        self.counts = array('Q', ( 0 for i in range(65536)) )
-        os.lseek(self.fd, 0, os.SEEK_SET)
 
-        while  (bytes:= os.read(self.fd, self.item_sz )) != b'':
+    def _count(self, fd):
+        self.counts = array('Q', (0 for i in range(65536)))
+        os.lseek(fd, 0, os.SEEK_SET)
+
+        while (bytes := os.read(fd, self.item_sz)) != b'':
             val = unpack(self.item_fmt, bytes)[0]
             self.counts[val] += 1
-       
 
-    def sort_with_cats(self):
+    def _sort_impl(self, fd, ls, rs):
         # count values
-        self._count()
-        
+        self._count(fd)
+
         # make indexes
         for i in range(1, len(self.counts)):
             self.counts[i] += self.counts[i-1]
 
         # reopen source file for random access and crate its copy to store sorted values
-        os.close(self.fd)
+        os.close(fd)
         tempfilename = f'{self.filename}.tmp'
-        os.system(f'copy {self.filename} {tempfilename}') # Windows command
-        self.fd = os.open(self.filename, flags=os.O_RDWR | os.O_BINARY | os.O_RANDOM )
-        fd_tmpfile = os.open(tempfilename, flags=os.O_RDWR | os.O_BINARY | os.O_RANDOM )
-        
+        os.system(f'copy {self.filename} {tempfilename} >NUL')  # Windows command
+        self.fd = os.open(self.filename, flags=os.O_RDWR |
+                          os.O_BINARY | os.O_RANDOM)
+        fd_tmpfile = os.open(tempfilename, flags=os.O_RDWR |
+                             os.O_BINARY | os.O_RANDOM)
+
         # writing values in places to temp file
-        for i in range(self.size-1, -1, -1): # [size-1, ... 0]
-            bytes = self._read_bytes_from(i)
+        for i in range(self.size-1, -1, -1):  # [size-1, ... 0]
+            bytes = self._read_bytes_from(fd, i)
             val = unpack(self.item_fmt, bytes)[0]
             self.counts[val] -= 1
             index = self.counts[val]
-
-            os.lseek(fd_tmpfile, index*self.item_sz, os.SEEK_SET)
-            os.write(fd_tmpfile, bytes)
+            self._write_bytes_to(fd_tmpfile, index, bytes)
 
         # replace original file with temp file
-        os.close(self.fd)
+        os.close(fd)
         os.close(fd_tmpfile)
-        os.system(f'move /Y {tempfilename} {self.filename}') # Windows command
+        os.system(f'move /Y {tempfilename} {self.filename} >NUL')  # Windows command
 
-        # just keep file open, while FileSort class is exists
+        # just keep file open, as it come in argument
         self.fd = os.open(self.filename, flags=os.O_RDWR | os.O_BINARY)
-        
 
-    def sort_without_cats(self):
+
+class FileSortCountingNoCats(FileSortCounting):
+    ''' File sort (in-place) with counting algo '''
+    def __init__(self, filename, item_size=2):
+        super().__init__(filename, item_size=item_size)
+
+    def _sort_impl(self, fd, rs, ls):
         # count values
-        self._count()
-        
-        # writing values in places 
-        os.lseek(self.fd, 0, os.SEEK_SET)
+        self._count(fd)
+        # writing values in places
+        os.lseek(fd, 0, os.SEEK_SET)
         for i in range(len(self.counts)):
             if self.counts[i] > 0:
                 bytes = pack(self.item_fmt, i)
-                os.write(self.fd, bytes * self.counts[i])
-                
+                os.write(fd, bytes * self.counts[i])
--- test_file_sort.py
+++ test_file_sort.py
@@ -2,20 +2,35 @@ import os
 from struct import pack, unpack
 from random import randint
 
-from file_sort import FileSortMerge, FileSortMergeHeap, FileSortCounting
+from file_sort import FileSortMerge, FileSortMergeHeap, FileSortCounting, FileSortCountingNoCats
 from timer import Log_profile
 
 
-def generate_u16_file(filename, size):
-    with open(filename, 'wb') as f:
-        for i in range(size):
-            f.write(pack('H', randint(0, 65535)))
+def get_unsorted_file(size):
+
+    def generate_u16_file(filename, size):
+        with open(filename, 'wb') as f:
+            for i in range(size):
+                f.write(pack('H', randint(0, 65535)))
+
+    fname = f'test_u16_{size//1000}k.bin'
+
+    if os.path.exists(f'{fname}.unsorted'):
+        #print("Restore unsorted file from backup")
+        os.system(f'copy /Y {fname}.unsorted {fname} >NUL')
+    else:
+        #print(f"Generating new unsorted file size = {size}")
+        generate_u16_file(fname, size)
+        os.system(f'copy {fname} {fname}.unsorted >NUL')
+
+    return fname
 
 
 def check_file_sorted(filename):
     ret = True
     v1 = (0, )
     v2 = (0, )
+    pos = 0
     with open(filename, 'rb') as f:
         while (v1 <= v2):
             buf = f.read(2)
@@ -23,42 +38,33 @@ def check_file_sorted(filename):
                 return True  # end of File
             v1 = v2
             v2 = unpack("H", buf)
+            pos += 1
+
+    print(f'Unsorted on pos {pos} with {hex(v1[0])} > {hex(v2[0])} ')
     return False
+
+
+def test_file_sort(FileSort, size):
+    fname = get_unsorted_file(size)
+    fsort = FileSort(fname)
+    print(f"\nSorting file with {size} items using {FileSort.__name__} ...", end='')
+    
+    with Log_profile("time:"):
+        fsort.sort()
+    
+    print(f"Checking file is sorted...", end='')
+    print(check_file_sorted(fname))
 
 
 if __name__ == "__main__":
 
-    size = 10**3
-    fname = f'test_u16_{size//1000}k.bin'
+    tests = [
+        ( FileSortMerge,  (10**4, 10**5, 10**6) ),
+        ( FileSortMergeHeap,  (10**4, 10**5, 10**6, 10**7) ),
+        ( FileSortCounting,  (10**4, 10**5, 10**6, 10**7) ),
+        ( FileSortCountingNoCats,  (10**4, 10**5, 10**6, 10**7, 10**8) )
+    ]
 
-    if os.path.exists(f'{fname}.unsorted'):
-        print("Restore unsorted file from backup")
-        os.system(f'copy {fname}.unsorted {fname}')
-    else:
-        print(f"Generating new unsorted file size = {size}")
-        generate_u16_file(fname, size)
-        print("Make file backup")
-        os.system(f'copy {fname} {fname}.unsorted')
-
-    print(f"Sort file...")
-
-    # fsort = FileSortMerge(fname)
-    # with Log_profile("Sort time"):
-    #     fsort.sort_without_cats()
-
-    # fsort = FileSortMergeHeap(fname)
-    # with Log_profile("Sort time"):
-    #     fsort.sort_without_cats()
-
-    # fsort = FileSortCounting(fname)
-    # with Log_profile("Sort time"):
-    #     fsort.sort_with_cats()
-
-    fsort = FileSortCounting(fname)
-    with Log_profile("Sort time"):
-        fsort.sort_without_cats()
-
-
-    print(f"Check result...")
-    result = check_file_sorted(fname)
-    print(f'Sorted file check result: {result}')
+    for sorter, rng in tests:
+        for size in rng:
+            test_file_sort(sorter, size)
