3e716f3aa92a0e31c63adc215cb144ef649796ae
Ampermetr123 <sb.simonov@gmail.com>
2022-02-04 23:41:46 +0300

    +BMH,BM

--- strings/find.py
+++ strings/find.py
@@ -1,10 +1,10 @@
+from typing import List
+
 # 1) Написать алгоритм поиска подстроки полным перебором, 2 байта.
 
 
 # Поиск в строке text подстроки patern
 # Возвращает индекс начала подстрки или -1, если подстрока отсутсвует
-
-
 def find_string_brute(patern: str, text: str) -> int:
     "Поиск полным перебором"
     t = 0  # index of text
@@ -21,6 +21,185 @@ def find_string_brute(patern: str, text: str) -> int:
 
 
 # 2) Оптимизировать алгоритм, используя сдвиги по префиксу шаблона, 2 байта.
+# Поиск в строке text подстроки patern
+# Возвращает индекс начала подстрки или -1, если подстрока отсутсвует
+def find_string_shift(patern: str, text: str) -> int:
+    """Смещение на 1 или длину шаблона"""
+
+    def make_shift_table(pat: str) -> List[int]:
+        # AABCDEAx......
+        #    ABCDE
+        #        ->ABCDE
+        psz = len(pat)
+        table = [psz] * 128
+
+        for i in range(psz - 1):
+            table[ord(pat[i])] = 1
+        return table
+
+    len_t, len_p = len(text), len(patern)
+    if len_p > len_t or len_p == 0:
+        return -1
+    shift = make_shift_table(patern)
+    t = len_p  # index of text
+    while t <= len_t:
+        # xxxxSTRINGxxxxx
+        #          ^ ->
+        #        (t-1)
+        #    PATTERN
+        #       <- ^
+        #          p=-1
+        p = 1  # index of patern
+        while p <= len_p and text[t - p] == patern[-p]:
+            p += 1
+        if p > len_p:
+            return t - len_p
+        t += shift[ord(text[t - 1])]
+    return -1
+
+
 # 3) Оптимизировать алгоритм, используя сдвиги по суффиксу текста, 2 байта.
+
+# Поиск в строке text подстроки patern
+# Возвращает индекс начала подстрки или -1, если подстрока отсутсвует
+def find_string_BMH(patern: str, text: str) -> int:
+    """Алгоритм Бойера — Мура — Хорспула"""
+
+    def make_shift_table(pat: str) -> List[int]:
+        psz = len(pat)
+        table = [psz] * 128
+        # ABCDAE
+        # 14321-
+        for i in range(psz - 1):
+            table[ord(pat[i])] = psz - 1 - i
+        return table
+
+    len_t, len_p = len(text), len(patern)
+    if len_p > len_t or len_p == 0:
+        return -1
+    shift = make_shift_table(patern)
+    t = len_p  # index of text
+    while t <= len_t:
+        # xxxxSTRINGxxxxx
+        #          ^ ->
+        #        (t-1)
+        #    PATTERN
+        #       <- ^
+        #          p=-1
+        p = 1  # index of patern
+        while p <= len_p and text[t - p] == patern[-p]:
+            p += 1
+        if p > len_p:
+            return t - len_p
+        t += shift[ord(text[t - 1])]
+    return -1
+
+
 # 4) Реализовать алгоритм Бойера-Мура, 2 байта.
-# 5)Протестировать работу алгоритмов для разных начальных данных, 1 байт.
+
+# Поиск в строке text подстроки patern
+# Возвращает индекс начала подстрки или -1, если подстрока отсутсвует
+def make_suffix_table(pat: str) -> List[int]:
+    """Строим таблицу, в которой индекс - размер суффикса,
+    значение, смещение шаблона для полного повтора этого суффикса
+    или для частичного повтора в начале строки
+    """
+    psz = len(pat)
+    table = [0] * psz
+    table[0] = psz
+
+    #           Example
+    # ABABC|AB           \\ patern (len=7)
+    #   <-  ^
+    #       i = 2  (AB)       \\  i = 1, 2, 3, 4, 5, 6
+    #  <- ^
+    #     j = len-i-1 = 4     \\  j = 4, 3, 2, 1, 0
+    for i in range(1, psz):
+        # Строка делится на две части индексом  i; i - размер суффикса.
+        # ищем совпадение суфикса в левой части шаблна;
+        for j in range(psz - i - 1, -1, -1):
+            k = 1  # index on suffix
+            # сравниваем справа налево суфикс и строку
+            while pat[j + 1 - k] == pat[-k] and k <= i and k <= j + 1:
+                k += 1
+            if k > i or k > j + 1:  # полное или частичное совпадение суффикса
+                table[i] = psz - j - 1
+                break
+        if table[i] == 0:  # cуффикс не был найден
+            table[i] = table[i - 1]
+    return table
+
+
+def find_string_BM(patern: str, text: str) -> int:
+    """Алгоритм Бойера — Мура"""
+
+    def make_shift_table(pat: str) -> List[int]:
+        psz = len(pat)
+        table = [psz] * 128
+        # ABCDAE
+        # 14321-
+        for i in range(psz - 1):
+            table[ord(pat[i])] = psz - 1 - i
+        return table
+
+    len_t, len_p = len(text), len(patern)
+    if len_p > len_t or len_p == 0:
+        return -1
+    shift_bad = make_shift_table(patern)  # таблица смещений, если суффикс не совпал
+    shift_good = make_suffix_table(patern)  # таблица смещений, если есть суффикс
+    t = len_p  # index of text
+    while t <= len_t:
+        # xxxxSTRINGxxxxx
+        #          ^ ->
+        #        (t-1)
+        #    PATTERN
+        #       <- ^
+        #          p=-1
+        p = 1  # index of patern
+        while p <= len_p and text[t - p] == patern[-p]:
+            p += 1
+        if p > len_p:
+            return t - len_p
+        if p == 1:
+            t += shift_bad[ord(text[t - 1])]
+        else:
+            t += shift_good[p - 1]
+    return -1
+
+
+if __name__ == "__main__":
+
+    # Проверка make_suffix_table(pat) для Боера-Мура
+
+    pat = "BC.ABC.BC.C.ABC"  # из лекции
+    print("pat =", pat)
+    table = make_suffix_table(pat)
+    assert table == [15, 4, 6, 9, 9, 9, 13, 13, 13, 13, 13, 13, 13, 13, 13]
+    print(table)
+
+    #           *
+    # =....xLOKOL......
+    #     KOLOKOL
+    #
+    # 1 L 4
+    # 2 OL 4
+    # 3 KOL 4
+    # 3 OKOL 4
+    # 4 LOKOL 4
+    # 5 OLOKOL 4
+    # -
+    pat = "KOLOKOL"
+    print("pat =", pat)
+    table = make_suffix_table(pat)
+    print(table)
+    assert table == [7, 4, 4, 4, 4, 4, 4]
+
+    pat = "They're putting"
+    print("pat =", pat)
+    table = make_suffix_table(pat)
+    print(table)
+
+    pat = "We say that Napoleon wished to invade Russia and invaded it."
+    print("pat =", pat)
+    table = make_suffix_table(pat)
+    print(table)
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,25 +1,55 @@
+import functools
+import os
+import sys
+
 import folder_test as ft
 import multi_func_tester as mft
 
-from strings import find_string_brute
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from strings import find_string_BM, find_string_BMH, find_string_brute, find_string_shift
+
+
+def repeat(func, n=1):
+    """Decorator to reapeat call function for n times"""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        for _ in range(n):
+            rv = func(*args, **kwargs)
+        return rv
+
+    return wrapper
+
 
 if __name__ == "__main__":
 
     test_folders = ("tests/data",)
 
+    rt = 20
+    # repeat times for each function call
+    rng = range(3, 5)  # диапазон тестов
+
     testing_func_list = [
-        (find_string_brute, range(5)),
+        (repeat(find_string_brute, rt), rng),
+        (repeat(find_string_shift, rt), rng),
+        (repeat(find_string_BMH, rt), rng),
+        (repeat(find_string_BM, rt), rng),
     ]
 
-    # def make_description_for_test(args):
-    #     arr_line_1 = args[0]
-    #     return str(f'N={arr_line_1[0]}')
+    def make_description_for_test(args):
+        plen = len(args[0])
+        slen = len(args[1])
+        return str(f"{plen} in {slen}")
 
-    tester = mft.MultiFuncFolderTester(testing_func_list, "Поиск в строке  ")
+    tester = mft.MultiFuncFolderTester(
+        testing_func_list, "Поиск в строке  ", make_description_for_test
+    )
 
     with open("report_all.md", "w", encoding="utf-8") as f:
-        # with open(sys.stdout, "w", encoding="utf-8") as f:
-        # f = sys.stdout
+
         f.write("# Поиск в строке \n\n")
         for folder_path in test_folders:
             folder_name = folder_path.split("/")[-1]
